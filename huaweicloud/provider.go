0d1d66ed5df3b24448539d6e3caebdff24752280
Lance52259 <Overlord52259@gmail.com>
2021-06-22 17:06:58 +0800

    feat: new apig environment resource support

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -365,6 +365,7 @@ func Provider() terraform.ResourceProvider {
 			"huaweicloud_api_gateway_group":               ResourceAPIGatewayGroup(),
 			"huaweicloud_apig_instance":                   ResourceApigInstanceV2(),
 			"huaweicloud_apig_application":                ResourceApigApplicationV2(),
+			"huaweicloud_apig_environment":                ResourceApigEnvironmentV2(),
 			"huaweicloud_as_configuration":                ResourceASConfiguration(),
 			"huaweicloud_as_group":                        ResourceASGroup(),
 			"huaweicloud_as_lifecycle_hook":               ResourceASLifecycleHook(),
